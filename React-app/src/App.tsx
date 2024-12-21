7793a246e7275af235b318fe5012bb2713bb4405
Prashant Arya <169087910+chhota-don@users.noreply.github.com>
2024-12-21 15:29:45 +0000

    first commit

---
+++ React-app/src/App.tsx
@@ -0,0 +1,10 @@
+import ListGroup from './components/ListGroup';
+import './App.css';
+
+function App() {
+let items = ["Patna", "Mumbai", "Kolkata", "Delhi"];
+
+ return <div><ListGroup items= {items} heading = "citis"/></div>;
+}
+
+export default App
